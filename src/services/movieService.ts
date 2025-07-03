72f06e46795c9529f807ef51d0721571c15e51ea
Elena Polyakova <epolyk1970@gmail.com>
2025-07-03 16:44:14 +0300

    token

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -16,7 +16,7 @@ export async function fetchMovies(query: string): Promise<Movie[]> {
       page: 1,
     },
     headers: {
-      Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4OGQ3N2IzOTQxYWJjNjM4NWIyYmZjNTg1NDc1NzhiOSIsIm5iZiI6MTc1MTMxMTgzNi44MTYsInN1YiI6IjY4NjJlNWRjMTVlZjE1NTZkYTA4MDlmMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.1BK-xx7A06czdrt7PEmyspODBzm5qH3h5hjC3WLNWno`,
+      Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`,
     },
   };
 
