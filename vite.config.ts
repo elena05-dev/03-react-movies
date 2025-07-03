310682752f1939145661c614fe336f046c269c3b
Elena Polyakova <epolyk1970@gmail.com>
2025-07-03 16:51:18 +0300

    conf

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  base: "/03-react-movies/",
-  plugins: [react()],
   build: {
+    outDir: "dist",
     sourcemap: true,
   },
+  plugins: [react()],
 });
