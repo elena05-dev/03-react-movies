feafb8e1b9e5e0185cb16e7aede5f876a79e4d96
Elena Polyakova <epolyk1970@gmail.com>
2025-07-05 11:09:59 +0300

    all

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv, type ConfigEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-export default defineConfig({
-  build: {
-    outDir: "dist",
-    sourcemap: true,
-  },
-  plugins: [react()],
-});
+export default (config: ConfigEnv) => {
+  const env = loadEnv(config.mode, process.cwd(), "");
+
+  return defineConfig({
+    base: env.VITE_BASE || "/",
+    build: {
+      outDir: env.VITE_OUT_DIR || "dist",
+      sourcemap: true,
+    },
+    plugins: [react()],
+  });
+};
